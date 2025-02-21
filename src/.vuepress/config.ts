4379de1af8f70903339de8030d21cc18ad2a2b4d
hippotumux <st6805972@gmail.com>
2025-02-21 11:20:54 +0000

    initcode

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,17 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "博客演示",
+  description: "vuepress-theme-hope 的博客演示",
+  
+  port: 7777,
+  theme,
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
